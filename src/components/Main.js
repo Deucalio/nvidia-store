27040d3ac016d38bd51707cc673bd6cdfc8f59fd
Deucalio <hjamad89@gmail.com>
2022-11-19 00:47:19 +0500

    add animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const IMAGES = [
   "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/csm_geforce_rtx_4080_product_photo_001_7_a4f5f5ba77.jpg",
@@ -8,24 +8,47 @@ const IMAGES = [
 ];
 
 const Main = () => {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  // colors the selected dot green
+  const colorDot = (dot) => {};
+
+  if (imgIndex === 4) {
+    setImgIndex(0);
+  }
+  useEffect(() => {
+    const changeImg = () => {
+      setImgIndex((i) => i + 1);
+    };
+
+    const timeOut = setTimeout(changeImg, 10000);
+
+    return () => {
+      clearInterval(timeOut);
+    };
+  }, [imgIndex]);
+
   return (
     <>
-      <main class="flex flex-col justify-center border-sky-400 bg-cover bg-center px-4 md:px-24 lg:bg-contain lg:bg-right-bottom lg:bg-no-repeat">
-        <div class="flex flex-col gap-5 w-48 md:w-2/5 lg:w-1/3 text-3xl border-red-500 font-bold text-white sm:-translate-y-11  leading-tight">
-          <p class=" lg:text-5xl">
+      <main
+        style={{ backgroundImage: `url(${IMAGES[imgIndex]})` }}
+        className={`duration-1000 transition-[opacity] opacity-${0} flex flex-col justify-center border-sky-400 bg-cover bg-center px-4 md:px-24 lg:bg-contain lg:bg-right-bottom lg:bg-no-repeat`}
+      >
+        <div className="flex flex-col gap-5 w-48 md:w-2/5 lg:w-1/3 text-3xl border-red-500 font-bold text-white sm:-translate-y-11  leading-tight">
+          <p className=" lg:text-5xl">
             "Spectacular eye-candy at sky-high frame rates"
           </p>
-          <p class="">- OC3D</p>
-          <button class="w-fit rounded-sm bg-[#76b900] px-3 py-1 text-xl text-white transition-all hover:bg-[#84c01a]">
+          <p className="">- OC3D</p>
+          <button className="w-fit rounded-sm bg-[#76b900] px-3 py-1 text-xl text-white transition-all hover:bg-[#84c01a]">
             Out Now
           </button>
         </div>
       </main>
-      <div class="mx-auto flex w-1/2 -translate-y-10 flex-wrap justify-center gap-2 border-amber-400 text-center text-6xl text-[#84c01a] xl:-translate-x-16">
-        <div class="h-3 w-3 cursor-pointer rounded-full bg-[#84c01a]"></div>
-        <div class="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
-        <div class="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
-        <div class="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
+      <div className="mx-auto flex w-1/2 -translate-y-10 flex-wrap justify-center gap-2 border-amber-400 text-center text-6xl text-[#84c01a] xl:-translate-x-16">
+        <div className="h-3 w-3 cursor-pointer rounded-full bg-[#84c01a]"></div>
+        <div className="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
+        <div className="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
+        <div className="h-3 w-3 cursor-pointer rounded-full bg-gray-400"></div>
       </div>
     </>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav class="h-14 bg-black px-7 py-2 md:px-24">
-      <ul class="flex items-center gap-10">
+    <nav className="h-14 bg-black px-7 py-2 md:px-24">
+      <ul className="flex items-center gap-10">
         <li>
-          <p class="cursor-pointer text-2xl font-bold text-white">GeForce</p>
+          <p className="cursor-pointer text-2xl font-bold text-white">GeForce</p>
         </li>
         <li>
-          <p class="mt-1 cursor-pointer text-xl text-gray-300 hover:text-gray-100">
+          <p className="mt-1 cursor-pointer text-xl text-gray-300 hover:text-gray-100">
             Products
           </p>
         </li>
